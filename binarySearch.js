88ea75ec5ba3b6e1c00f60039db1ae26c300879a
JashanB <jashan_brar50@hotmail.com>
2020-11-07 15:56:06 -0500

    2nd attempt

--- binarySearch.js
+++ binarySearch.js
@@ -1,25 +1,29 @@
-const exampleArr = [0, 1, 2, 3, 4, 5]
+const exampleArr = [0, 1, 2, 3, 4, 5];
 //at what index does x element appear in a list
 //sweeping - start from beginnning to end - O(n)
 //memory usage and speed are things to consider
 //note only works if the list is ordered
 //our guess is below
 
-const binarySearch = function (arr, elementToFind) {
+const binarySearch = function (arr, elementToFind, previousSearchedIndex) {
   const arrLength = arr.length;
-  //length/2 - 1 to get middle 
   const searchIndex = Math.round((arrLength/2) -1);
-  // if else statements to see if higher, lower, or equal
+  let cumluativeIndex = searchIndex + previousSearchedIndex;
   if (arr[searchIndex] < elementToFind) {
-    const slicedArr = arr.slice(searchIndex, arrLength);
-    binarySearch(slicedArr, elementToFind);
+    cumluativeIndex += 1;
+    const slicedArr = arr.slice((searchIndex+1), arrLength);
+    binarySearch(slicedArr, elementToFind, cumluativeIndex);
   } else if (arr[searchIndex] > elementToFind) {
+    cumluativeIndex -= 1;
     const slicedArr = arr.slice(0, searchIndex);
-    binarySearch(slicedArr, elementToFind);
-  } else if (arr[searchIndex] === elementToFind) {
-    return searchIndex;
+    binarySearch(slicedArr, elementToFind, cumluativeIndex);
+  } else if (arr[searchIndex] === elementToFind || arr.length === 1) {
+    // cumluativeIndex += 1;
+    return cumluativeIndex;
   } else {
-    return 'Element not contained in array'
+    return 'Element not contained in array';
   };
-};
+}
+let answer = binarySearch(exampleArr, 1, 0);
+console.log(answer);
 
