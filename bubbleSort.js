e3d40ac3aabe688362590be1bbc8e95577f35cd8
JashanB <jashan_brar50@hotmail.com>
2020-11-08 15:23:40 -0500

    bubbles highest digit but dosent sort fully

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,3 +1,11 @@
-const bubbleSort = function (array) {
-  
+const bubbleSort = function (inputArray) {
+  let highest
+  for (let i = 0; i < inputArray.length -1; i++) {
+    if (inputArray[i] > inputArray[i+1]) {
+      highest = inputArray[i];
+      inputArray[i] = inputArray[i+1];
+      inputArray[i+1] = highest;
+    }
+  }
+  return inputArray;
 }
