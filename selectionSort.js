123d8214a24fbdaf4c1320b6f1b7f2e95bf9cf49
JashanB <jashan_brar50@hotmail.com>
2020-11-08 15:05:37 -0500

    all tests passed

--- selectionSort.js
+++ selectionSort.js
@@ -1,3 +1,16 @@
-const selectionSort = fucntion (array) {
-  
+const selectionSort = function (array) {
+  let copyArr = array
+  const newArr = [];
+  let lowest = array[0];
+  while (newArr.length < array.length) {
+    for (let element of copyArr) {
+      if (element < lowest) {
+        lowest = element;
+      }
+    }
+    newArr.push(lowest);
+    copyArr = copyArr.filter(x => x!== lowest);
+    lowest = copyArr[0];
+  }
+  return newArr;
 }
