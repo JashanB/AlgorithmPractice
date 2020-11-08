705396fa8a757440c8d7d2036dd1751de64f4683
JashanB <jashan_brar50@hotmail.com>
2020-11-08 15:27:07 -0500

    passes all tests but n^2

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,15 @@
 const bubbleSort = function (inputArray) {
   let highest
-  for (let i = 0; i < inputArray.length -1; i++) {
-    if (inputArray[i] > inputArray[i+1]) {
-      highest = inputArray[i];
-      inputArray[i] = inputArray[i+1];
-      inputArray[i+1] = highest;
+  let j = 0
+  while (j < inputArray.length) {
+    for (let i = 0; i < inputArray.length -1; i++) {
+      if (inputArray[i] > inputArray[i+1]) {
+        highest = inputArray[i];
+        inputArray[i] = inputArray[i+1];
+        inputArray[i+1] = highest;
+      }
     }
+    j++
   }
   return inputArray;
 }
