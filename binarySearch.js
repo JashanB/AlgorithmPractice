bb57834aa8d3ef7c9eabadf5542c7e3c9e276ee9
JashanB <jashan_brar50@hotmail.com>
2020-11-07 19:59:12 -0500

    binary with recursion

--- binarySearch.js
+++ binarySearch.js
@@ -5,27 +5,7 @@ const exampleArr = [0, 1, 2, 3, 4, 5];
 //note only works if the list is ordered
 //our guess is below
 
-// const binarySearch = function (arr, elementToFind, previousSearchedIndex) {
-//   const arrLength = arr.length;
-//   const searchIndex = Math.round((arrLength/2) -1);
-//   let cumluativeIndex = searchIndex + previousSearchedIndex;
-//   if (arr[searchIndex] < elementToFind) {
-//     cumluativeIndex += 1;
-//     const slicedArr = arr.slice((searchIndex+1), arrLength);
-//     binarySearch(slicedArr, elementToFind, cumluativeIndex);
-//   } else if (arr[searchIndex] > elementToFind) {
-//     cumluativeIndex -= 1;
-//     const slicedArr = arr.slice(0, searchIndex);
-//     binarySearch(slicedArr, elementToFind, cumluativeIndex);
-//   } else if (arr[searchIndex] === elementToFind || arr.length === 1) {
-//     // cumluativeIndex += 1;
-//     return cumluativeIndex;
-//   } else {
-//     return 'Element not contained in array';
-//   };
-// }
-// let answer = binarySearch(exampleArr, 1, 0);
-// console.log(answer);
+
 let defaultCompare = (a, b) =>
   a > b ? 1 : (a < b ? -1 : 0);
   
@@ -34,18 +14,35 @@ let defaultCompare = (a, b) =>
     Fixed memory
     Loops
 */
-let binarySearchWithLoops = (array, element, compare = defaultCompare) => {
-  let left = 0;
-  let right = array.length - 1;
-  while (left <= right) {
-    let middle = Math.floor((left + right /2));
-    if (compare(array[middle], element) === 1) {
-      right = middle -1;
-    } else if (compare(array[middle], element) === -1) {
-      left = middle +1;
-    } else if ((compare(array[middle], element) === 0)) {
-      return middle;
-    }
+// let binarySearchWithLoops = (array, element, compare = defaultCompare) => {
+//   let left = 0;
+//   let right = array.length - 1;
+//   while (left <= right) {
+//     let middle = Math.floor((left + right /2));
+//     if (compare(array[middle], element) === 1) {
+//       right = middle -1;
+//     } else if (compare(array[middle], element) === -1) {
+//       left = middle +1;
+//     } else if ((compare(array[middle], element) === 0)) {
+//       return middle;
+//     }
+//   }
+//   return -1;
+// };
+
+let binarySearchWithRecursion = (array, element, compare = defaultCompare, left = 0, right = array.length - 1) => {
+  if (left > right) {
+    return -1;
+  };
+  const middle = Math.floor((right + left/2));
+  if (defaultCompare(array[middle], element) === 1) {
+    binarySearchWithRecursion(array, element, compare, left , middle -1);
+  } else if (defaultCompare(array[middle], element) === -1) {
+    binarySearchWithRecursion(array, element, compare, middle +1, right);
+  } else if (defaultCompare(array[middle], element) === 0) {
+    return middle;
   }
   return -1;
 };
+
+export default binarySearchWithRecursion;
